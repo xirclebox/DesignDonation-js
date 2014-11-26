cd9c3679b6d1379c92aa0051e590430315a20cf6
Homer Gaines <xirclebox@gmail.com>
2014-11-26 17:02:55 -0500

    Separated the clicks

--- designDonation.js
+++ designDonation.js
@@ -7,110 +7,98 @@
  *
  *
  * */
+$(document).ready(function() {
 
+            var activeTopNavBtn;
+            var activeSectionBtn;
+            var newPos;
+            var activeBtn;
+            var pleaseWait;
 
-$(document).ready(function () {
+            $('#nav a').click(function() {
+                $('#nav a').removeClass('active');
+                $(this).addClass('active');
+                activeTopNavBtn = $(this).attr('id');
+                goHere();
+                return false;
+            });
+    
+            $('#logo').click(function(){
+                activeTopNavBtn = $('#about-Link').attr('id');
+                goHere();
+                return false;
+            })
 
-    var activeTopNavBtn;
-    var activeSectionBtn;
-    var newPos;
-    var activeBtn;
-    var pleaseWait;
+            function goHere() {
+                if (activeTopNavBtn == 'about-Link') {
 
-    $('#nav a, #logo').click(function () {
+                    newPos = "about";
 
-        $('#nav a').removeClass('active');
-        $('#' + activeTopNavBtn).addClass('active');
+                } else if (activeTopNavBtn == 'projects-Link') {
 
-        activeTopNavBtn = $(this).attr('id');
+                    newPos = "projects";
 
-        if (activeTopNavBtn == 'logo') {
+                } else if (activeTopNavBtn == 'help-Link') {
 
-            activeTopNavBtn = $('#about-Link').attr('id');
-            return false;
+                    newPos = "footer";
 
-        } else {
+                }
 
-            activeTopNavBtn = $(this).attr('id');
-            return false;
+                $('html, body').animate({
+                    scrollTop: $("#" + newPos).offset().top - 50
+                }, 1000, 'easeInOutQuart', function() {});
 
-        }
-
-        goHere();
-
-    });
-
-
-
-    function goHere() {
-
-        if (activeTopNavBtn == 'about-Link') {
-
-            newPos = "about";
-
-        } else if (activeTopNavBtn == 'projects-Link') {
-
-            newPos = "projects";
-
-        } else if (activeTopNavBtn == 'help-Link') {
-
-            newPos = "footer";
-
-        }
-
-        $('html, body').animate({
-            scrollTop: $('#' + newPos).offset().top - 50
-        }, 1000, 'easeInOutQuart', function () {});
-
-    }
+            }
 
 
 
-    $(window).on('scroll', function () {
-        var y_scroll_pos = window.pageYOffset;
-        var topNavIsThere = $('#nav').is(':visible');
+            $(window).on('scroll', function() {
+                var y_scroll_pos = window.pageYOffset;
+                var scroll_pos_check = 0; // set to whatever you want it to be
+                var topNavIsThere = $('#nav').is(':visible');
 
-        if (y_scroll_pos > 0 && topNavIsThere) {
+                if (y_scroll_pos > scroll_pos_check && topNavIsThere) {
 
-            $('#header').addClass('shortHeader');
-            $('#logo').addClass('shortLogo');
-            $('#nav').addClass('shortNav');
+                    $('#header').addClass('shortHeader');
+                    $('#logo').addClass('shortLogo');
+                    $('#nav').addClass('shortNav');
 
-        } else if (y_scroll_pos < 250) {
+                } else if (y_scroll_pos < 250) {
 
-            $('#header').removeClass('shortHeader');
-            $('#nav').removeClass('shortNav');
-            pleaseWait = window.setTimeout(delayTallLogo, 100);
+                    $('#header').removeClass('shortHeader');
+                    $('#nav').removeClass('shortNav');
+                    pleaseWait = window.setTimeout(delayTallLogo, 100);
+                    
+                }
 
-        }
+                /* */
 
-        /* */
+                if (y_scroll_pos <= 498) {
 
-        if (y_scroll_pos <= 498) {
+                    activeBtn = "about-Link"
 
-            activeBtn = "about-Link"
+                } else if (y_scroll_pos > 500 && y_scroll_pos < 710) {
 
-        } else if (y_scroll_pos > 500 && y_scroll_pos < 710) {
+                    activeBtn = "projects-Link"
 
-            activeBtn = "projects-Link"
+                } else if (y_scroll_pos >= 800) {
 
-        } else if (y_scroll_pos >= 800) {
+                    activeBtn = "help-Link"
 
-            activeBtn = "help-Link"
-
-        }
-
-        $("#" + activeBtn).addClass('active');
+                }
 
 
-    });
+                $('#nav a').removeClass('active');
+                $("#" + activeBtn).addClass('active');
 
-    function delayTallLogo() {
-        $('#logo').removeClass('shortLogo');
-    }
+            });
 
-    var thisYear = new Date();
-    var copyRightYr = thisYear.getFullYear();
-    $('#copyright').html("Â© 2010 - " + copyRightYr + " Homer Gaines");
+            function delayTallLogo(){
+                $('#logo').removeClass('shortLogo');
+            }
+    
+            var thisYear = new Date();
+            var copyRightYr = thisYear.getFullYear();
+            $('#copyright').html("© 2010 - " + copyRightYr + " Homer Gaines");
 
-})
+        })
